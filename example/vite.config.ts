b6835d15fe8f97c3512dec936302f6b967fbf822
Stanley Horwood <stanley.horwood@hotmail.fr>
2022-03-16 12:03:38 +0000

    simpler glob

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
 
       custom: {
         families: {
-          'Dancing Script': './assets/fonts/DancingScript-*.ttf',
+          'Dancing Script': './assets/fonts/DancingScript*',
         },
       },
     }),
